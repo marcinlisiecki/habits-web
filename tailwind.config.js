639f6adcd660d82b72091ccead4b53f741720ebd
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-09-16 14:08:37 +0200

    Add tailwind theme config & poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,49 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    backgroundColor: {
+      ...colors,
+      dark: '#090C10',
+      light: '#0D1117',
+      primary: {
+        DEFAULT: '#3B82F6',
+        50: '#EFF6FF',
+        100: '#DBEAFE',
+        200: '#BFDBFE',
+        300: '#93C5FD',
+        400: '#60A5FA',
+        500: '#3B82F6',
+        600: '#2563EB',
+        700: '#1D4ED8',
+        800: '#1E40AF',
+        900: '#1E3A8A',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        ...colors,
+        primary: {
+          DEFAULT: '#3B82F6',
+          50: '#EFF6FF',
+          100: '#DBEAFE',
+          200: '#BFDBFE',
+          300: '#93C5FD',
+          400: '#60A5FA',
+          500: '#3B82F6',
+          600: '#2563EB',
+          700: '#1D4ED8',
+          800: '#1E40AF',
+          900: '#1E3A8A',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};

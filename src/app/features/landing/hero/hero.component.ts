a884af41b7a9a2c9354ca167fc6c4db35a38dc93
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-09-17 13:38:41 +0200

    Remove learn more button

--- src/app/features/landing/hero/hero.component.ts
+++ src/app/features/landing/hero/hero.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent {
-
+  protected readonly window = window;
 }
